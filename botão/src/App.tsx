0ff7e084d968b393d3f4e0fe004dc9f308456671
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2025-01-08 21:51:51 -0300

    uso de botões no PrimeReact

---
+++ botão/src/App.tsx
@@ -0,0 +1,24 @@
+import {Button} from "primereact/button";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeflex/primeflex.css";
+import 'primeicons/primeicons.css';
+
+function App() {
+  return (
+    <>
+      <div className=" container   mt-5 p-5 ">
+        <Button label="clica aqui" icon="pi pi-check" className="p-button-primary mx-2 "/>
+        <Button label="clica aqui" icon="pi pi-check" className="p-button-success mx-2 "/>
+        <Button label="clica aqui" icon="pi pi-check" className="p-button-danger mx-2"/>
+        <Button label="clica aqui" icon="pi pi-check" className="p-button-help mx-2"/>
+        <Button label="clica aqui" icon="pi pi-check" className="p-button-text mx-2"/>
+        <Button label="clica aqui" icon="pi pi-check" className="p-button-info mx-2"/>
+        <Button label="clica aqui" icon="pi pi-check" className="p-button-secondary mx-2"/>
+        <Button label="clica aqui" icon="pi pi-check" className="p-button-warning mx-2"/>
+        <Button label="clica aqui" icon="pi pi-check" className="p-button-outlined mx-2 rounded" />
+      </div>
+    </>
+  );
+}
+
+export default App;
